c0d716ba1e2dafd97256bb3f66e1ee78090e7dfe
Solomon B. Braimah <150222143+SBedeB@users.noreply.github.com>
2024-01-22 16:23:55 +0400

    Update index.js

--- index.js
+++ index.js
@@ -2,13 +2,28 @@
 // where your node app starts
 
 // init project
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+const cors = require('cors');
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
+
+// Middleware for the API endpoint
+const nowMiddleware = (req, res, next) => {
+  req.time = new Date().toString();
+  next();
+};
+
+// Root-Level Request Logger Middleware
+app.use((req, res, next) => {
+  const method = req.method;
+  const path = req.path;
+  const ip = req.ip;
+  console.log(`${method} ${path} - ${ip}`);
+  next();
+});
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -18,15 +33,41 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+// Middleware to handle timestamp requests
+app.use("/api/:date?", (req, res, next) => {
+  const dateString = req.params.date;
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  if (!dateString) {
+    // If the date parameter is empty, use the current time
+    req.parsedDate = new Date();
+  } else if (!isNaN(dateString)) {
+    // If the date is a valid Unix timestamp, parse it
+    req.parsedDate = new Date(parseInt(dateString));
+  } else {
+    // Check if the date is valid
+    const parsedDate = new Date(dateString);
+    if (!isNaN(parsedDate.getTime())) {
+      req.parsedDate = parsedDate;
+    } else {
+      // If the date is invalid, return an error
+      return res.json({ error: "Invalid Date" });
+    }
+  }
+
+  next();
 });
 
+// Route handler for the timestamp endpoint
+app.get("/api/:date?", nowMiddleware, (req, res) => {
+  const unixTimestamp = req.parsedDate.getTime();
+  const utcString = req.parsedDate.toUTCString();
 
+  res.json({ unix: unixTimestamp, utc: utcString });
+});
 
-// listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+// listen for requests
+const listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
+module.exports = app;
